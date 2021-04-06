c3f02e28b60228ab13389e0ec7df514c3bfed465
kince <kince@micous.com>
2021-04-06 10:17:13 +0800

    ViewPager库初始化

---
+++ app/src/main/java/com/geekince/widget/viewpagerx/demo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.geekince.widget.viewpagerx.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.geekince.widget.viewpagerx.demo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
